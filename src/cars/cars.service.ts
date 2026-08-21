13efb81787cffbc04a95c30c182042b631423e41
ajang20 <ajangkorenilo8@gmail.com>
2026-08-21 22:39:16 +0200

    feat: made changes

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,15 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
+import { Car } from './entities/car.entity';
 
 @Injectable()
 export class CarsService {
-  create(createCarDto: CreateCarDto) {
-    return 'This action adds a new car';
+  constructor(
+    @InjectRepository(Car)
+    private readonly carRepository: Repository<Car>,
+  ) {}
+
+  async create(createCarDto: CreateCarDto) {
+    try {
+      const newCar = this.carRepository.create(createCarDto);
+      return await this.carRepository.save(newCar);
+    } catch (error: unknown) {
+      if (
+        error &&
+        typeof error === 'object' &&
+        'code' in error &&
+        error.code === '23505'
+      ) {
+        throw new ConflictException(
+          `A car with registration number ${createCarDto.registrationNumber} already exists`,
+        );
+      }
+      throw new InternalServerErrorException(
+        'Database Error: Unable to create car record',
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all cars`;
+  async findAll() {
+    return await this.carRepository.find();
   }
 
   findOne(id: number) {
