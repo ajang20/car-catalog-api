c06c0ea3e6d9a4326fa8d074b0425c2b7cb6c296
ajang20 <ajangkorenilo8@gmail.com>
2026-08-21 22:38:15 +0200

    feat: implemented the cardto

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,1 +1,35 @@
-export class CreateCarDto {}
+import {
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+  Max,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+export class CreateCarDto {
+  @IsString()
+  @Transform(({ value }): string =>
+    typeof value === 'string' ? value.toUpperCase().trim() : value,
+  )
+  registrationNumber!: string;
+
+  @IsString()
+  make!: string;
+
+  @IsString()
+  model!: string;
+
+  @IsInt()
+  @Min(1886)
+  year!: number;
+
+  @IsInt()
+  @Min(1)
+  @Max(12)
+  seats!: number;
+
+  @IsOptional()
+  @IsBoolean()
+  isAvailable?: boolean = true;
+}
--- src/cars/entities/car.entity.ts
+++ src/cars/entities/car.entity.ts
@@ -1,1 +1,44 @@
-export class Car {}
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Timestamp } from 'typeorm/driver/mongodb/bson.typings.js';
+
+@Entity('cars')
+export class Car {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true })
+  registrationNumber!: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  normalizeRegistration() {
+    if (this.registrationNumber) {
+      this.registrationNumber = this.registrationNumber.toUpperCase().trim();
+    }
+  }
+
+  @Column()
+  make!: string;
+
+  @Column()
+  model!: string;
+
+  @Column()
+  year!: number;
+
+  @Column()
+  seats!: number;
+
+  @Column({ default: true })
+  isAvailable!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Timestamp;
+}
